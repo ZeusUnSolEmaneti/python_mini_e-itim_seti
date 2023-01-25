dc749145672687078cd8962ba17534845923ff7a
ZeusUnSolEmaneti <123559846+ZeusUnSolEmaneti@users.noreply.github.com>
2023-01-25 15:59:41 +0300

    Create 8-Veri Yapıları.py

---
+++ 8-Veri Yapıları.py
@@ -0,0 +1,13 @@
+#string
+#integer
+#float
+#bool
+
+
+#Pythonda belirli veri tipleri vardır bunlar bizim yaptığımız işleri kolaylaştırır ve değişkenlere ne yazarsak ona göre bir veri tipi atanır. ÖRN:
+
+sayi=1 #bu bir integer veri tipidir integer veri tipi nedir? tam sayılardır 
+isim="mehmet" #bu bir string veri tipidir peki string nedir? direkt olarak kelime veya cümleler demektir aslında veya karakterler
+ondalıklı_sayı=56.7 #bu bir float veri tipidir float veri tipi nedir? float veri tipi ondalıklı sayı demektir
+deger=True #bu bir booling veri tipidir peki bool veri tipi nedir? doğru ve yanlış için yapılmıs olan bir veri tipidir bu veri tipi doğru yada yanlışı belirtir
+
